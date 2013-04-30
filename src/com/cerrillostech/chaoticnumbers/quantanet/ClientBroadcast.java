e474220fea9ca1a036e05aa6a7294919764ab7cd
Carlos C <carlos@cerrillostech.com>
2013-04-29 19:22:43 -0700

    Set the broadcast client & server to use the new QuantaPacket system, and completed the handshake methods that send the server a challenge, need to continue on what to do after a handshake is completed.

--- src/com/cerrillostech/chaoticnumbers/quantanet/ClientBroadcast.java
+++ src/com/cerrillostech/chaoticnumbers/quantanet/ClientBroadcast.java
@@ -7,10 +7,15 @@ import java.net.InetAddress;
 import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
 import java.util.Enumeration;
+import java.util.Random;
+
+import com.cerrillostech.chaoticnumbers.quantanet.QuantaPacket.Header;
+import com.cerrillostech.chaoticnumbers.quantanet.QuantaPacket.Type;
 
 public class ClientBroadcast extends Thread {
 	private DatagramSocket sock;
 	private InetAddress foundServer;
+	private int chaAns;
 	public void run(){
 		System.setProperty("java.net.preferIPv4Stack" , "true");
 		openSocket();
@@ -33,11 +38,57 @@ public class ClientBroadcast extends Thread {
 	}
 	public void handshake(){
 		System.out.println("Attempting handshake with: "+foundServer.getHostAddress());
+		Random ran = new Random();
+		chaAns = ran.nextInt(50000);
+		int cha = ((chaAns+42)*24);
+		QuantaPacket chapac = new QuantaPacket();
+		chapac.setHeader(Header.CHALLENGE);
+		chapac.setType(Type.REQUEST);
+		chapac.setDataType(Type.INTEGER);
+		chapac.setData(cha+"");
+		chapac.setFromType(Type.CLIENT);
+		chapac.setToType(Type.SERVER);
+		sendChallenge(chapac);
+	//	QuantaPacket
 		//Send challenge
 		// :SH:C_A:EH:
 	}
+	public void sendChallenge(QuantaPacket qp){
+		byte[] data = qp.toString().getBytes();
+		try{
+			DatagramPacket packet = new DatagramPacket(data, data.length, this.foundServer, 50411);
+			sock.send(packet);
+			System.out.println("Challenge Request sent to: "+this.foundServer.getHostAddress());
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		try{
+			byte[] buffer = new byte[16000];
+			DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length);
+			sock.receive(receivePacket);
+			System.out.println("Challenge response from server: " + receivePacket.getAddress().getHostAddress());
+			String message = new String(receivePacket.getData()).trim();
+			QuantaPacket cr = new QuantaPacket(message);
+			if(cr.isChallenge() && cr.typeIs(Type.RESPONSE) && cr.fromTypeIs(Type.SERVER) && cr.toTypeIs(Type.CLIENT) && cr.getDataType().equals(Type.INTEGER)){
+				int response = Integer.parseInt(cr.getData());
+				if(response==chaAns){
+					System.out.println("Challenge completed!");
+				} else {
+					System.out.println("Challege answered incorrectly!!");
+				}
+			}
+		} catch (Exception e){
+			System.out.println("Challenge Time out! Sending new challenge!");
+			handshake();
+		}
+	}
 	public void broadcast(){
-		byte[] data = "QUANTANET_CHAOTICNUMBERS_CLIENT_REQUEST".getBytes();
+		QuantaPacket reqpac = new QuantaPacket();
+		reqpac.setHeader(Header.DISCOVERY);
+		reqpac.setType(Type.REQUEST);
+		reqpac.setFromType(Type.CLIENT);
+		reqpac.setToType(Type.SERVER);
+		byte[] data = reqpac.toString().getBytes();
 		try{
 			DatagramPacket packet = new DatagramPacket(data, data.length, InetAddress.getByName("255.255.255.255"), 50411);
 			sock.send(packet);
@@ -54,12 +105,12 @@ public class ClientBroadcast extends Thread {
 				}
 				for(InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()){
 					InetAddress broadcast = interfaceAddress.getBroadcast();
-					if(broadcast == null){
-						continue;
+					if(broadcast != null){
+						DatagramPacket packet = new DatagramPacket(data, data.length, broadcast, 50411);
+						sock.send(packet);
+						System.out.println("Request packet sent to: " + broadcast.getHostAddress() + "; Interface: " + networkInterface.getDisplayName());
 					}
-					DatagramPacket packet = new DatagramPacket(data, data.length, broadcast, 50411);
-					sock.send(packet);
-					System.out.println("Request packet sent to: " + broadcast.getHostAddress() + "; Interface: " + networkInterface.getDisplayName());
+					
 				}
 			}
 			System.out.println("Finished broadcasting on all interfaces.\nWaiting for reply...");
@@ -83,7 +134,12 @@ public class ClientBroadcast extends Thread {
 	}
 	public void respondToReply(String message, DatagramPacket receivePacket, int count){
 		if(count!=3){
-			if(message.equals("QUANTANET_CHAOTICNUMBERS_CLIENT_RESPONSE")){
+			QuantaPacket qpp = new QuantaPacket();
+			qpp.setHeader(Header.DISCOVERY);
+			qpp.setType(Type.RESPONSE);
+			qpp.setFromType(Type.SERVER);
+			qpp.setToType(Type.CLIENT);
+			if(message.equals(qpp.toString())){
 				System.out.println("Server: " + receivePacket.getAddress().getHostAddress() + " has been validated!");
 				this.foundServer = receivePacket.getAddress();
 				handshake();
--- src/com/cerrillostech/chaoticnumbers/quantanet/ServerBroadcast.java
+++ src/com/cerrillostech/chaoticnumbers/quantanet/ServerBroadcast.java
@@ -4,6 +4,9 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
+import com.cerrillostech.chaoticnumbers.quantanet.QuantaPacket.Header;
+import com.cerrillostech.chaoticnumbers.quantanet.QuantaPacket.Type;
+
 public class ServerBroadcast extends Thread {
 	private DatagramSocket sock;
 	public void run(){
@@ -34,12 +37,34 @@ public class ServerBroadcast extends Thread {
 				System.out.println("Received packet from: " + packet.getAddress().getHostAddress());
 				System.out.println("Data: " + new String(packet.getData()).trim());
 				String message = new String(packet.getData()).trim();
-				if(message.equals("QUANTANET_CHAOTICNUMBERS_CLIENT_REQUEST")){
-					byte[] data = "QUANTANET_CHAOTICNUMBERS_CLIENT_RESPONSE".getBytes();
+				QuantaPacket rpac = new QuantaPacket(message);
+				if(rpac.isDiscovery() && rpac.fromTypeIs(Type.CLIENT) && rpac.toTypeIs(Type.SERVER) && rpac.typeIs(Type.REQUEST)){
+					QuantaPacket qp = new QuantaPacket();
+					qp.setHeader(Header.DISCOVERY);
+					qp.setType(Type.RESPONSE);
+					qp.setFromType(Type.SERVER);
+					qp.setToType(Type.CLIENT);
+					byte[] data = qp.toString().getBytes();
 					DatagramPacket responsePacket = new DatagramPacket(data, data.length, packet.getAddress(), packet.getPort());
 					sock.send(responsePacket);
 					System.out.println("Sent response to: " + responsePacket.getAddress().getHostAddress());
-					
+					System.out.println("Data: "+qp.toString());
+				}
+				if(rpac.isChallenge() && rpac.typeIs(Type.REQUEST) && rpac.fromTypeIs(Type.CLIENT) && rpac.toTypeIs(Type.SERVER) && rpac.dataTypeIs(Type.INTEGER)){
+					int challenge = Integer.parseInt(rpac.getData());
+					int response = (int) ((challenge/24)-42);
+					QuantaPacket resp = new QuantaPacket();
+					resp.setHeader(Header.CHALLENGE);
+					resp.setType(Type.RESPONSE);
+					resp.setFromType(Type.SERVER);
+					resp.setToType(Type.CLIENT);
+					resp.setDataType(Type.INTEGER);
+					resp.setData(response+"");
+					byte[] data = resp.toString().getBytes();
+					DatagramPacket respacket = new DatagramPacket(data, data.length, packet.getAddress(), packet.getPort());
+					sock.send(respacket);
+					System.out.println("Sent Challenge response to: " + respacket.getAddress().getHostAddress());
+					System.out.println("Data: "+resp.toString());
 				}
 			}
 		//	closeSocket();
