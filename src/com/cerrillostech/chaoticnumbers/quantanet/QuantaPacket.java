3031d2b665622b647b56820e68d3abe816503ccc
Carlos C <carlos@cerrillostech.com>
2013-04-29 19:20:58 -0700

    Continued to expand on the QuantaPackets

--- src/com/cerrillostech/chaoticnumbers/quantanet/QuantaPacket.java
+++ src/com/cerrillostech/chaoticnumbers/quantanet/QuantaPacket.java
@@ -35,7 +35,7 @@ public class QuantaPacket {
 		}
 	}
 	public enum Type {
-		CLIENT("CLI"), SERVER("SER"), REQUEST("REQ"), RESPONSE("RES"), INVALID("INV");
+		CLIENT("CLI"), SERVER("SER"), REQUEST("REQ"), RESPONSE("RES"), INVALID("INV"), INTEGER("INT"), STRING("STR"), DOUBLE("DOU");
 		private final String value;
 		private Type(String value){
 			this.value = value;
@@ -52,6 +52,12 @@ public class QuantaPacket {
 				return Type.CLIENT;
 			} else if(in.equals("SER")){
 				return Type.SERVER;
+			} else if(in.equals("INT")){
+				return Type.INTEGER;
+			} else if(in.equals("STR")){
+				return Type.STRING;
+			} else if(in.equals("DOU")){
+				return Type.DOUBLE;
 			}
 			return Type.INVALID;
 		}
@@ -64,7 +70,7 @@ public class QuantaPacket {
 		}
 	}
 	public enum Prefix {
-		HEADER("HEA"), FROMTYPE("FROT"), TOTYPE("TOT"), TYPE("TYP"), BLANK(" ");
+		HEADER("HEA"), FROMTYPE("FROT"), TOTYPE("TOT"), TYPE("TYP"), BLANK(" "), DATA("DAT");
 		private final String value;
 		private Prefix(String value){
 			this.value = value;
@@ -74,58 +80,97 @@ public class QuantaPacket {
 		}
 		public static Prefix parse(String in){
 			if(in.equals("HEA")){
-				
+			//	return 
 			} else if(in.equals("FROT")){
 				
 			}
 			return Prefix.BLANK;
 		}
 	}
-	private final Header header;
-	private final Type type, fromType, toType;
+	/** end enums */
+	private Header header;
+	private Type type, fromType, toType, dataType;
+	private String data;
+	public QuantaPacket(){
+		this.header = Header.INVALID;
+		this.type = Type.INVALID;
+		this.fromType = Type.INVALID;
+		this.toType = Type.INVALID;
+		this.dataType = Type.INVALID;
+	}
 	public QuantaPacket(Header header, Type type){
 		this.header = header;
 		this.type = type;
 		this.fromType = Type.INVALID;
 		this.toType = Type.INVALID;
+		this.dataType = Type.INVALID;
 	}
 	public QuantaPacket(String packetData){
 		Header header = Header.INVALID;
 		Type type = Type.INVALID;
 		Type fromType = Type.INVALID;
 		Type toType = Type.INVALID;
+		Type dataType = Type.INVALID;
 		String[] splitStrings = packetData.split("\\|");
 		for(int x = 0; x < splitStrings.length; x++){
-			if(splitStrings[x].equals("HEA")){
+			if(splitStrings[x].equals(Prefix.HEADER.value)){
 				header = Header.parse(splitStrings[x+1]);
 			}
-			if(splitStrings[x].equals("FROT")){
+			if(splitStrings[x].equals(Prefix.FROMTYPE.value)){
 				fromType = Type.parse(splitStrings[x+1]);
 			}
-			if(splitStrings[x].equals("TOT")){
+			if(splitStrings[x].equals(Prefix.TOTYPE.value)){
 				toType = Type.parse(splitStrings[x+1]);
 			}
 			if(splitStrings[x].equals("CT")){
 				
 			}
-			if(splitStrings[x].equals("TYP")){
+			if(splitStrings[x].equals(Prefix.TYPE.value)){
 				type = Type.parse(splitStrings[x+1]);
+			}
+			if(splitStrings[x].equals(Prefix.DATA.value)){
+				dataType = Type.parse(splitStrings[x+1]);
+				this.data = splitStrings[x+2];
 			}
 		}
 		this.header = header;
 		this.type = type;
 		this.fromType = fromType;
 		this.toType = toType;
+		this.dataType = dataType;
 	}
-	
+	public void setDataType(Type type){
+		this.dataType = type;
+	}
+	public Type getDataType(){
+		return this.dataType;
+	}
+	public void setData(String data){
+		this.data = data;
+	}
+	public String getData(){
+		return this.data;
+	}
+	public void setHeader(Header head){
+		this.header = head;
+	}
 	public Header getHeader(){
 		return this.header;
+	}
+	public void setType(Type type){
+		this.type = type;
 	}
 	public Type getType(){
 		return this.type;
 	}
+	public void setFromType(Type type){
+		this.fromType = type;
+	}
 	public Type getFromType(){
 		return this.fromType;
+	}
+	public void setToType(Type type){
+		this.toType = type;
 	}
 	public Type getToType(){
 		return this.toType;
@@ -144,7 +189,43 @@ public class QuantaPacket {
 		if(toType.isValid()){
 			output = output+Prefix.TOTYPE.toString()+"|"+this.toType.toString()+"|";
 		}
+		if(dataType.isValid()){
+			output = output+Prefix.DATA.toString()+"|"+this.dataType.toString()+"|"+this.data+"|";
+		}
 		return output;
+	}
+	public boolean toTypeIs(Type type){
+		if(this.toType.equals(type)){
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public boolean fromTypeIs(Type type){
+		if(this.fromType.equals(type)){
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public boolean typeIs(Type type){
+		if(this.type.equals(type)){
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public boolean headerIs(Header head){
+		if(this.header.equals(head)){
+			return true;
+		}
+		return false;
+	}
+	public boolean dataTypeIs(Type type){
+		if(this.dataType.equals(type)){
+			return true;
+		}
+		return false;
 	}
 	public boolean isBroadcast(){
 		if(this.header.equals(Header.BROADCAST)){
@@ -152,5 +233,42 @@ public class QuantaPacket {
 		} else {
 			return false;
 		}
+	}
+	public boolean isDiscovery(){
+		if(this.header.equals(Header.DISCOVERY)){
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public boolean isAccept(){
+		if(this.header.equals(Header.ACCEPT)){
+			return true;
+		}
+		return false;
+	}
+	public boolean isDecline(){
+		if(this.header.equals(Header.DECLINE)){
+			return true;
+		}
+		return false;
+	}
+	public boolean isResend(){
+		if(this.header.equals(Header.RESEND)){
+			return true;
+		}
+		return false;
+	}
+	public boolean isChallenge(){
+		if(this.header.equals(Header.CHALLENGE)){
+			return true;
+		}
+		return false;
+	}
+	public boolean isInvalid(){
+		if(this.header.equals(Header.INVALID)){
+			return true;
+		}
+		return false;
 	}
 }
